96f91030196dc42b14546dcc4438267f456876a5
ginosian <marta.ginosian@gmail.com>
2017-11-14 10:02:33 +0400

    Adds security success handler.

--- src/main/java/com/attendance_manager/handler/SecuritySuccessHandler.java
+++ src/main/java/com/attendance_manager/handler/SecuritySuccessHandler.java
@@ -1,6 +1,9 @@
 package com.attendance_manager.handler;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.DefaultRedirectStrategy;
 import org.springframework.security.web.RedirectStrategy;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
@@ -9,46 +12,77 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Collection;
+import java.util.Iterator;
 
 /**
  * @author Marta Ginosyan
  * Date: 10/22/17
  */
+@PropertySource("classpath:security.properties")
 public class SecuritySuccessHandler  extends SimpleUrlAuthenticationSuccessHandler {
+
+    @Value("${path_base_login}")
+    String path_base_login;
+    @Value("${path_base_logout}")
+    String path_base_logout;
+    @Value("${key_remember_me}")
+    String key_remember_me;
+    @Value("${key_username}")
+    String key_username;
+    @Value("${key_password}")
+    String key_password;
+    @Value("${path_base}")
+    String path_base;
+    @Value("${path_base_extended}")
+    String path_base_extended;
+    @Value("${role_admin}")
+    String role_admin;
+    @Value("${role_employee}")
+    String role_employee;
+    @Value("${role_hr}")
+    String role_hr;
+    @Value("${admin}")
+    String admin;
+    @Value("${employee}")
+    String employee;
+    @Value("${hr}")
+    String hr;
+    @Value("${base_path_admin}")
+    String base_path_admin;
+    @Value("${base_path_hr}")
+    String base_path_hr;
+    @Value("${base_path_employee}")
+    String base_path_employee;
 
     private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
 
     @Override
     protected void handle(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-
         if (response.isCommitted()) {
             return;
         }
         try {
-            redirectStrategy.sendRedirect(request, response, SecuritySuccessHandler.resolveTargetURL(authentication, PATH_BASE));
+            redirectStrategy.sendRedirect(request, response, resolveTargetURL(authentication, path_base));
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
 
-    public static String resolveTargetURL(Authentication authentication, String noAuthenticatedPath) {
+    private String resolveTargetURL(Authentication authentication, String noAuthenticatedPath) {
         String targetUrl = noAuthenticatedPath;
         Collection<? extends GrantedAuthority> grantedAuthorities = authentication.getAuthorities();
         Iterator<? extends GrantedAuthority> iterator = grantedAuthorities.iterator();
         while (iterator.hasNext()) {
             GrantedAuthority grantedAuthority = iterator.next();
-            if (grantedAuthority.getAuthority().equalsIgnoreCase(Constants.ROLE_ADMIN))
-                targetUrl = Constants.PATH_BASE_ADMIN;
-            else if (grantedAuthority.getAuthority().equalsIgnoreCase(Constants.ROLE_CANDIDATE))
-                targetUrl = Constants.PATH_BASE_CANDIDATE;
-            else if (grantedAuthority.getAuthority().equalsIgnoreCase(Constants.ROLE_COMPANY))
-                targetUrl = Constants.PATH_BASE_COMPANY;
+            if (grantedAuthority.getAuthority().equalsIgnoreCase(role_admin))
+                targetUrl = base_path_admin;
+            else if (grantedAuthority.getAuthority().equalsIgnoreCase(role_employee))
+                targetUrl = base_path_employee;
+            else if (grantedAuthority.getAuthority().equalsIgnoreCase(role_hr))
+                targetUrl = base_path_hr;
         }
-
-        LOG.warn("User: " + authentication.getName()
-                + " granted access to the protected URL: "
-                + targetUrl);
         return targetUrl;
     }
 }
